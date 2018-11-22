372f792f3a7d4f08d25521954f62e7a2808c09e8
busterace <busterace@163.com>
2018-11-22 16:42:59 +0800

    覆盖上传勾选后可以直接上传

--- test/demo1/uploadH5.js
+++ test/demo1/uploadH5.js
@@ -41,14 +41,15 @@ function initFileAdd(file) {
     $("#h5uploadFileTable tbody").append(htmlstr);
 };
 
-function initFile(file) {
-
+function getToken(file) {
     var getTokenUrlId = $("#getTokenUrlId").val();
     var expire = new Date();
     expire.setDate(expire.getDate()+1);//设置token有效期1天
+    var token = null;
     $.ajax({
         url: getTokenUrlId,
         type: "POST",
+        async: false,
         data: {
             ak: $("#akId").val(),
             bucket: $("#bucketNameId").val(),
@@ -58,78 +59,90 @@ function initFile(file) {
         },
         error:function(res){
             alert("token 获取失败");
+            token =  null;
         },
         success: function (res) {
-            console.log(res);
-            var token = res;
-
-            var uploadUrl = $("#uploadUrlId").val();
-
-            var extraConfig = {
-                timeout: parseInt($("#timeout").val()),
-                concurrentRequestLimit: parseInt($("#concurrentRequestLimit").val()),
-                retryCount: parseInt($("#retryCount").val())
-            };
-
-            var uploadObj = wcs.wcsUpload(file, token, uploadUrl, extraConfig);
-
-            console.log(uploadObj);
-
-
-            initFileAdd(uploadObj.file);
-            var li = $("#h5uploadFileTable tr[data-fileid='" + uploadObj.file.id + "']");
-            uploadObj.uploadProgress = function (progress) {
-                console.log("uploadProgress");
-                console.log(progress);
-
-                if (progress.chunks) {
-
-                    if ($(li).find(".chunk_process_contain").length == 0) {
-
-                        $(li).find(".process_contain").after("<div>块进度</div><div class='chunk_process_contain'></div>");
-                        var splitPercent = 100 / progress.chunks.length;
-                        for (var i = 0; i < progress.chunks.length; i++) {
-                            $(li).find(".chunk_process_contain").append("<span class=\"process_bar\" width='"+splitPercent+"%'><span></span></span>");
-                        }
-                    }
-
-                    var chunkProcessSpan =   $(li).find(".chunk_process_contain .process_bar span");
-                    for (var i = 0; i < progress.chunks.length; i++) {
-                        var chunk = progress.chunks[i];
-                        var chunkPercent = chunk.percent.toFixed(2) + "%";
-                        $($(chunkProcessSpan).get(i)).css("width", chunkPercent);
-                    }
-                }
-                console.log(progress.total.percent);
-                $(li).find(".process_contain .process_bar span").css("width", progress.total.percent.toFixed(2) + "%");
-                $(li).find(".process_num").html(progress.total.percent.toFixed(2)+"%");
-
-            };
-
-            uploadObj.onError = function (error) {
-                $(li).find(".message").html(error.message);
-            };
-
-            uploadObj.onComplete = function(res){
-                console.log(res);
-                $(li).find(".message").html("上传成功");
-            };
-
-            $(li).find(".upload_start").on("click", function () {
-                uploadObj.putFile();
-                $(li).find(".message").html("");
-            });
-
-            $(li).find(".upload_stop").on("click", function () {
-                uploadObj.stop();
-            });
-
-            $(li).find(".J_upfile_delete").on("click", function () {
-                $(li).remove();
-
-            });
-
+            token =  res;
         }
     });
+
+    return token;
+}
+
+function initFile(file) {
+    var token = getToken(file);
+    console.log(token);
+
+    if (token == null) {
+        return;
+    }
+
+    var uploadUrl = $("#uploadUrlId").val();
+
+    var extraConfig = {
+        timeout: parseInt($("#timeout").val()),
+        concurrentRequestLimit: parseInt($("#concurrentRequestLimit").val()),
+        retryCount: parseInt($("#retryCount").val())
+    };
+
+    var uploadObj = wcs.wcsUpload(file, token, uploadUrl, extraConfig);
+
+    console.log(uploadObj);
+
+
+    initFileAdd(uploadObj.file);
+    var li = $("#h5uploadFileTable tr[data-fileid='" + uploadObj.file.id + "']");
+    uploadObj.uploadProgress = function (progress) {
+        console.log("uploadProgress");
+        console.log(progress);
+
+        if (progress.chunks) {
+
+            if ($(li).find(".chunk_process_contain").length == 0) {
+
+                $(li).find(".process_contain").after("<div>块进度</div><div class='chunk_process_contain'></div>");
+                var splitPercent = 100 / progress.chunks.length;
+                for (var i = 0; i < progress.chunks.length; i++) {
+                    $(li).find(".chunk_process_contain").append("<span class=\"process_bar\" width='"+splitPercent+"%'><span></span></span>");
+                }
+            }
+
+            var chunkProcessSpan =   $(li).find(".chunk_process_contain .process_bar span");
+            for (var i = 0; i < progress.chunks.length; i++) {
+                var chunk = progress.chunks[i];
+                var chunkPercent = chunk.percent.toFixed(2) + "%";
+                $($(chunkProcessSpan).get(i)).css("width", chunkPercent);
+            }
+        }
+        console.log(progress.total.percent);
+        $(li).find(".process_contain .process_bar span").css("width", progress.total.percent.toFixed(2) + "%");
+        $(li).find(".process_num").html(progress.total.percent.toFixed(2)+"%");
+
+    };
+
+    uploadObj.onError = function (error) {
+        $(li).find(".message").html(error.message);
+    };
+
+    uploadObj.onComplete = function(res){
+        console.log(res);
+        $(li).find(".message").html("上传成功");
+    };
+
+    $(li).find(".upload_start").on("click", function () {
+        uploadObj.token = getToken(file);
+        uploadObj.putFile();
+        $(li).find(".message").html("");
+    });
+
+    $(li).find(".upload_stop").on("click", function () {
+        uploadObj.stop();
+    });
+
+    $(li).find(".J_upfile_delete").on("click", function () {
+        $(li).remove();
+
+    });
+
 }
 
